fb379362f1c4b68d402fd58d4ab4c906207095e8
YeahZ <yeahz66@163.com>
2017-04-08 23:10:48 +0800

    update main.js

--- www/src/main.js
+++ www/src/main.js
@@ -15,19 +15,24 @@ Vue.use(VueRouter)
 Vue.use(Pagenav)
 
 const routes = [
-	{ path: '/', component: Blogs},
-	{ path: '/home', component: Blogs},
-	{ path: '/blogs/:blogId', component: Detial },
-	{ path: '/about', component: About },
-	{ path: '/category', component: Category },
-	{ path: '/tag', component: Tag },
-	{ path: '/pigeonhole', component: Pigeonhole },
-	{ path: '/*', component: NotFound }
+	{ path: '/', component: Blogs, meta: {title: 'YeahZ博客'} },
+	{ path: '/home', component: Blogs, meta: {title: '首页 YeahZ博客'} },
+	{ path: '/blogs/:blogId', component: Detial, meta: {title: '文章详情 YeahZ博客'} },
+	{ path: '/about', component: About, meta: {title: '关于 YeahZ博客'} },
+	{ path: '/category', component: Category, meta: {title: '分类 YeahZ博客'} },
+	{ path: '/tag', component: Tag, meta: {title: '标签 YeahZ博客'} },
+	{ path: '/pigeonhole', component: Pigeonhole, meta: {title: '归档 YeahZ博客'} },
+	{ path: '/*', component: NotFound, meta: {title: 'Error 404 YeahZ博客'} }
 ]
 
 const router = new VueRouter({
 	mode: 'history',
   routes 
+})
+
+router.beforeEach((to, from, next) => {
+	document.title = to.meta.title;
+  next()
 })
 
 new Vue({
